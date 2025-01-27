4e9d628045413d127331f8684b632310c2f363e0
Yohannes Taye <ytaye715@gmail.com>
2025-01-27 23:48:19 +0300

    update

--- utils/preprocess.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def stack_frames(frames, new_frame):
-    if frames is None:
-        return np.stack([new_frame] * 4, axis=-1)  # Stack 4 frames
-    else:
-        return np.concatenate([frames[..., 1:], new_frame[..., np.newaxis]], axis=-1)
